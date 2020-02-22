ab057ccebd1593d1a563d2b1ec70fc5591c85448
jwx524 <1318104160@qq.com>
2020-02-22 22:05:46 +0800

    1st-edition

---
+++ Practice 1/TCP多线程/TCPThread.java
@@ -0,0 +1,37 @@
+package TCP多线程;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TCPThread extends Thread{
+	Socket clientSocket = null;
+	TCPThread(Socket s){
+		clientSocket=s;
+	}
+	public void run() {
+		
+		try {
+			InputStream inStream = clientSocket.getInputStream();
+			OutputStream outStream = clientSocket.getOutputStream();
+			BufferedReader in = new BufferedReader(new InputStreamReader(inStream));
+			PrintWriter out = new PrintWriter(outStream);
+			
+			String line = null;
+			while((line=in.readLine())!=null) {
+				System.out.println("Message from client:" + line);
+				out.println(line);
+				out.flush();
+			}
+			clientSocket.close();
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		
+	}
+}
